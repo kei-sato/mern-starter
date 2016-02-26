dcf4a2c55a98893b1fd772ad3fb67356e171414c
Kei Sato <kei-sato@meeetol.com>
2016-02-26 23:04:40 +0900

    rename renderPage to render

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ require('./config/axios')();
 require('./config/webpack')(app);
 require('./config/express')(app);
 require('./routes/post.routes')(app);
-require('./routes/renderPage')(app);
+require('./routes/render')(app);
 
 // start app
 app.listen(serverConfig.port, (error) => {
